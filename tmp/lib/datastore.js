183ac0a9b8599cc1d84daf2ff8c9442d74087e81
iida_shigeu <iida_shigeu@cyberagent.co.jp>
2014-02-19 18:22:31 +0900

    add editorconfig

--- tmp/lib/datastore.js
+++ tmp/lib/datastore.js
@@ -1,12 +1,12 @@
 var getData = function(key_name) {
-	
-	var data = $.cookie(key_name);
-	if (data === undefined) {
-		return {};
-	}
-	return JSON.parse(data);
+
+  var data = $.cookie(key_name);
+  if (data === undefined) {
+    return {};
+  }
+  return JSON.parse(data);
 };
 
 var setData = function(key_name, data) {
-	$.cookie(key_name, JSON.stringify(data), { expires: 365, path: '/', json: true });
+  $.cookie(key_name, JSON.stringify(data), { expires: 365, path: '/', json: true });
 };
